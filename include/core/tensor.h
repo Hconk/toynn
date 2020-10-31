1d3bdcfbe22f5a05992d8380c46f35c6a5bb16d7
朱永康 <1164588530@qq.com>
2020-10-31 23:39:20 +0800

    add 2dim tensor init from vector<vector>

--- include/core/tensor.h
+++ include/core/tensor.h
@@ -42,29 +42,67 @@ namespace toynn
             return m_data;
         }
         tensor<T, dim>(const std::initializer_list<T>);
-        //tensor<T, dim>(const std::vector<T> &);
+        tensor<T, dim>(const std::vector<T> &);
 
-        ~tensor<T, dim>();
+        ~tensor<T, dim>() {}
+    };
+
+    template <typename T>
+    class tensor<T, 2>
+    {
+    private:
+        uint8_t m_dims;
+        uint8_t m_type;
+        uint8_t m_layout;
+        static const uint8_t m_dim_num = 2;
+        uint8_t m_elem_num;
+
+        std::shared_ptr<T[]> m_data;
+
+    public:
+        uint8_t dims() const
+        {
+            return m_dim_num;
+        }
+
+        std::shared_ptr<T[]> data() const
+        {
+            return m_data;
+        }
+        tensor<T, 2>(const std::initializer_list<T>) {}
+        tensor<T, 2>(const std::vector<std::vector<T>>);
+        //tensor<T>(const std::vector<T> &);
+
+        ~tensor<T, 2>() {}
     };
 
     template <typename T, uint8_t dims>
-    tensor<T, dims>::tensor(const std::initializer_list<T> ilist)
+    tensor<T, dims>::tensor(const std::initializer_list<T> ilist) : tensor<T, 1>(std::vector<T>(ilist))
     {
-        m_data = std::shared_ptr<T[]>(new T[ilist.size()]);
-        std::copy(ilist.begin(), ilist.end(), m_data.get());
     }
 
-    /*
     template <typename T, uint8_t dims>
     tensor<T, dims>::tensor(const std::vector<T> &data)
     {
+        m_data = std::shared_ptr<T[]>(new T[data.size()]);
+        std::copy(data.begin(), data.end(), m_data.get());
     }
-    */
 
-    template <typename T, uint8_t dims>
-    tensor<T, dims>::~tensor()
+    template <typename T>
+    tensor<T, 2>::tensor(const std::vector<std::vector<T>> ilist)
     {
+        int h = ilist.size();
+        int w = ilist.begin()->size();
+        m_data = std::shared_ptr<T[]>(new T[w * h]);
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                m_data[i * w + j] = ilist[i][j];
+            }
+        }
     }
+
 } // namespace toynn
 
 #endif
--- tests/tensor_test.cpp
+++ tests/tensor_test.cpp
@@ -16,7 +16,7 @@
 #include <ops/add.h>
 #include <core/tensor.h>
 
-TEST(tensor_test, tensor_init_from_vector_1dim)
+TEST(tensor_test, tensor_init_from_initializer_list_1dim)
 {
     std::vector<int> vi = {1, 2, 3, 4, 5, 6};
     toynn::tensor<int> tensor_int_1dim = {1, 2, 3, 4, 5, 6};
@@ -25,5 +25,34 @@ TEST(tensor_test, tensor_init_from_vector_1dim)
     for (size_t i = 0; i < vi.size(); i++)
     {
         EXPECT_EQ(vi[i], data.get()[i]);
+    }
+}
+
+TEST(tensor_test, tensor_init_from_vector_1dim)
+{
+    std::vector<int> vi = {1, 2, 3, 4, 5, 6};
+    toynn::tensor<int> tensor_int_1dim = vi;
+    EXPECT_EQ(tensor_int_1dim.dims(), 1);
+    auto data = tensor_int_1dim.data();
+    for (size_t i = 0; i < vi.size(); i++)
+    {
+        EXPECT_EQ(vi[i], data.get()[i]);
+    }
+}
+
+TEST(tensor_test, tensor_init_from_vector_2dim)
+{
+    std::vector<std::vector<int>> vi = {{1, 2, 3}, {4, 5, 6}};
+    toynn::tensor<int, 2> tensor_int_2dim = vi;
+    EXPECT_EQ(tensor_int_2dim.dims(), 2);
+    auto data = tensor_int_2dim.data();
+    int h = vi.size();
+    int w = vi[0].size();
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            EXPECT_EQ(data.get()[i * w + j], vi[i][j]);
+        }
     }
 }
